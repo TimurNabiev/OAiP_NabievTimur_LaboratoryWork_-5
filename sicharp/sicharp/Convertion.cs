15f7dcc22d62c1edd14cd17599053f8f4af3d374
TimurNabiev <timurnabiev805@gmail.com>
2026-01-20 21:17:21 +0300

    Первый коммит

---
+++ sicharp/sicharp/Convertion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sicharp { 
+    public class Convertion
+{
+            public int Time { get; set; }
+            public int Price {  get; set; }
+            public int cost()
+            {
+                return Price * Time;
+            }
+    }
+}
---
+++ sicharp/sicharp/Program.cs
@@ -0,0 +1,9 @@
+﻿using sicharp;
+Console.WriteLine("Введите время разговора в минутах:");
+int time = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите стоимость 1 минуты разговора:");
+int price = int.Parse(Console.ReadLine());
+Convertion call = new Convertion();
+call.Time = time;
+call.Price = price;
+Console.WriteLine($"Цена вашего разговора {call.cost()}");
